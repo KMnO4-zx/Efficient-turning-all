49aac0244481cffb4a7f5011ccb1a90b6f5d71a3
KMnO4-zx <1021385881@qq.com>
2023-07-19 18:33:34 +0800

    init lora

---
+++ lora/llm/src/api_demo.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+# Implements API for fine-tuned models in OpenAI's format. (https://platform.openai.com/docs/api-reference/chat)
+# Usage: python api_demo.py --model_name_or_path path_to_model --checkpoint_dir path_to_checkpoint
+# Visit http://localhost:8000/docs for document.
+
+import uvicorn
+
+from llmtuner import create_app
+
+
+if __name__ == "__main__":
+    app = create_app()
+    uvicorn.run(app, host="0.0.0.0", port=8000, workers=1)
